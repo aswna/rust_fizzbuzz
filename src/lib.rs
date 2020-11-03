3c045975e8aa09e149177d5a50779a19f26b4b8e
aswna <aswna@server.fake>
2020-11-03 09:32:02 +0100

    Finish fizzbuzz with TDD

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,62 @@
 pub fn fizzbuzz(max_number: i32) -> String {
     println!("Printing fizzbuzz up to: {}", max_number);
     let mut results = Vec::new();
-    for i in 1..max_number+1 {
-        results.push(i.to_string());
+    for i in 1..=max_number {
+        let result = number_to_fizzbuzz(i);
+        results.push(result);
     }
     return results.join(", ");
 }
 
+fn number_to_fizzbuzz(number: i32) -> String {
+    if number % 15 == 0 {
+        return "fizzbuzz".to_string();
+    } else if number % 3 == 0 {
+        return "fizz".to_string();
+    } else if number % 5 == 0 {
+        return "buzz".to_string();
+    } else {
+        return number.to_string();
+    }
+}
+
+// fn number_to_fizzbuzz(number: i32) -> String {
+//     let mut result = String::new();
+//     if number % 3 == 0 {
+//         result += "fizz";
+//     }
+//     if number % 5 == 0 {
+//         result += "buzz";
+//     }
+//     if result == "" {
+//         result = number.to_string();
+//     }
+//     return result;
+// }
+
 #[cfg(test)]
 mod tests {
-    // use super::*;
-    //
-    // #[test]
-    // fn test_fizzbuzz_with_1() {
-    //     assert_eq!(, 3);
-    // }
+    use super::*;
+
+    #[test]
+    fn test_number_to_fizzbuzz() {
+        let test_data = vec![
+            (1, "1"),
+            (2, "2"),
+            (3, "fizz"),
+            (4, "4"),
+            (5, "buzz"),
+            (6, "fizz"),
+            (9, "fizz"),
+            (10, "buzz"),
+            (12, "fizz"),
+            (15, "fizzbuzz"),
+            (30, "fizzbuzz"),
+            (99, "fizz"),
+            (100, "buzz"),
+        ];
+        for (number, expected_output) in test_data.iter() {
+            assert_eq!(expected_output.to_string(), number_to_fizzbuzz(*number));
+        }
+    }
 }
--- tests/test_fizzbuzz.rs
+++ tests/test_fizzbuzz.rs
@@ -19,4 +19,13 @@ mod tests {
         let output = rust_fizzbuzz::fizzbuzz(3);
         assert_eq!("1, 2, fizz", output);
     }
+
+    #[test]
+    fn test_fizzbuzz_outputs_for_input_15() {
+        let output = rust_fizzbuzz::fizzbuzz(15);
+        assert_eq!(
+            "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz",
+            output
+        );
+    }
 }
